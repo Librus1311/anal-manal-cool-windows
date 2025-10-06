b2a0d9af8b19b40b603562fe7dd602e96dea96ac
Librus1311 <straxolud2015@gmail.com>
2025-10-06 19:01:15 +0300

    light theme except change theme btn

--- script.js
+++ script.js
@@ -1,25 +1,42 @@
 window.addEventListener("load", () => {
     const toggleButton = document.querySelector('.theme-color');
     const body = document.body;
+    const header = document.querySelector('header')
     const cardItem = document.querySelectorAll('.extensions-cards-item');
-    // const cardItemTex = document.querySelector('.extensions-cards-item-text')
-    const removeBtn = document.querySelector('.remove');
-    const cartItemTitle = document.querySelector('.extensions-cards-item-text-title');
-    const cardItemText = document.querySelector('.extensions-cards-item-text-p');
+    const removeBtn = document.querySelectorAll('.remove');
+    const cardItemTitle = document.querySelectorAll('.extensions-cards-item-text-title');
+    const cardItemText = document.querySelectorAll('.extensions-cards-item-text-p');
+    const navBtn = document.querySelectorAll('.nav-btn')
+    const headerTitle = document.querySelector('.extensions-bar-title')
+    const mainTitle = document.querySelector('.header-title')
+    const themeBtn = document.querySelector('.theme-color')
+    
 
 
 
     // Функция переключения темы
     function toggleTheme() {
         body.classList.toggle('light-theme');
-        cardItem.classList.toggle('light-theme');
-        // cardItemTex.classList.toggle('light-theme');
-        removeBtn.classList.toggle('light-theme');
-        cartItemTitle.classList.toggle('light-theme');
-        cardItemText.classList.toggle('light-theme');
-
-
-
+        header.classList.toggle('light-theme');
+        headerTitle.classList.toggle('light-theme')
+        themeBtn.classList.toggle('light-theme')
+        mainTitle.classList.toggle('light-theme')
+        
+        cardItem.forEach(item => {
+            item.classList.toggle('light-theme')
+        })
+        cardItemTitle.forEach(item => {
+            item.classList.toggle('light-theme')
+        })
+        cardItemText.forEach(item => {
+            item.classList.toggle('light-theme')
+        })
+        removeBtn.forEach(item => {
+            item.classList.toggle('light-theme')
+        })
+        navBtn.forEach(item => {
+            item.classList.toggle('light-theme')
+        })
     }
 
     // Обработчик события
